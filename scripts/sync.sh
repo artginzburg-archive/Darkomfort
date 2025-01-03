018fe70b869e9e98b39ed74dde800a783987d07e
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-01-03 15:15:13 +0400

    fix: clean on sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,1 +1,2 @@
+rm -r ~/Library/Application\ Support/ModrinthApp/profiles/Main/resourcepacks/Darkomfort\ v2/ ; \
 cp -R $PWD/build/dist/ ~/Library/Application\ Support/ModrinthApp/profiles/Main/resourcepacks/Darkomfort\ v2/
