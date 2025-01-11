d880747cbfb87c9e7e62f169d2e6921b843846d9
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-01-11 08:51:27 +0400

    chore: update profile path

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,2 +1,2 @@
-rm -r ~/Library/Application\ Support/ModrinthApp/profiles/Main/resourcepacks/Darkomfort\ v2/ ; \
-cp -R $PWD/build/dist/ ~/Library/Application\ Support/ModrinthApp/profiles/Main/resourcepacks/Darkomfort\ v2/
+rm -r ~/Library/Application\ Support/ModrinthApp/profiles/Main\ \(1\)/resourcepacks/Darkomfort\ v2/ ; \
+cp -R $PWD/build/dist/ ~/Library/Application\ Support/ModrinthApp/profiles/Main\ \(1\)/resourcepacks/Darkomfort\ v2/
