47f609dbc7be2113029f9082510e2c81070fdac8
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-01-12 07:54:07 +0400

    feat: override CTM connected coal_ore textures

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,1 +1,1 @@
-find ./dist/ ! -path '*temporary hidden*' -name '*.png' -o -name '*.mcmeta' -o -name '*.json' -o -name 'splashes.txt' | cpio -p -dumv ./build/
+find ./dist/ ! -path '*temporary hidden*' -name '*.png' -o -name '*.mcmeta' -o -name '*.json' -o -name 'splashes.txt' -o -name '*.properties' | cpio -p -dumv ./build/
